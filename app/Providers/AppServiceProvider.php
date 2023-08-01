05b831419c536b425605fb66ed62f3ce699296ee
hassan <hassan@thiqatech.com>
2023-08-02 00:03:55 +0400

    logo changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+        App::setLocale('ar');
     }
 }
