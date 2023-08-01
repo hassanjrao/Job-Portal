f0d38035bac5eccd349d080390d04d7532b662e7
hassan <hassan@thiqatech.com>
2023-08-01 22:49:55 +0400

    logo changed

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -66,18 +66,18 @@
                 </div>
             </div>
 
-            {{-- <div class="row"> --}}
+            <div class="row">
 
-                {{-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9764394862584545"
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9764394862584545"
                     crossorigin="anonymous"></script>
                 <!-- square ad -->
                 <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9764394862584545"
                     data-ad-slot="4866663033" data-ad-format="auto" data-full-width-responsive="true"></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script> --}}
+                </script>
 
-            {{-- </div> --}}
+            </div>
 
             <div class="row items-push">
 
