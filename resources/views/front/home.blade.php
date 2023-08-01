007ca6f302b29ca0f1c7e7089ffc4c618043f8a2
hassan <hassan@thiqatech.com>
2023-08-01 22:49:13 +0400

    logo changed

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -68,14 +68,14 @@
 
             {{-- <div class="row"> --}}
 
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9764394862584545"
+                {{-- <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9764394862584545"
                     crossorigin="anonymous"></script>
                 <!-- square ad -->
                 <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9764394862584545"
                     data-ad-slot="4866663033" data-ad-format="auto" data-full-width-responsive="true"></ins>
                 <script>
                     (adsbygoogle = window.adsbygoogle || []).push({});
-                </script>
+                </script> --}}
 
             {{-- </div> --}}
 
